f96236c9dd654bc5a0757949124791ab94e7d5d0
D'Artagnan Blenke <dartagnan.blenke17@gmail.com>
2021-10-20 22:11:23 -0300

    listagem

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import { HighlightCard } from '../../components/HighlightCard';
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+} from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{ uri: 'https://avatars.githubusercontent.com/u/69226024?v=4'}} 
+            />
+            <User>
+              <UserGreeting>olá,</UserGreeting>
+              <UserName>D'Artagnan</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power"/> 
+
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard 
+          type="up"
+          title="Entradas" 
+          amount="R$ 17.400,00" 
+          lastTransaction="Última entrada dia 13 de abril" 
+        />
+        <HighlightCard 
+          type="down"
+          title="Saídas" 
+          amount="R$ 1.259,00" 
+          lastTransaction="Última entrada dia 13 de abril" 
+        />
+        <HighlightCard 
+          type="total"
+          title="Tota;" 
+          amount="R$ 16.141,00" 
+          lastTransaction="01 à 16 de abril" 
+        />
+        
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+      </Transactions>
+
+    </Container>
+  )
+}
